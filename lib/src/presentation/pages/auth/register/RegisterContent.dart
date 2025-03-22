c62ed2cdeda465c809388570f56a96d587e0abc7
sreyes <sreyes@capi.lat>
2025-03-21 18:08:48 -0600

    Creación de pantalla de registro

---
+++ lib/src/presentation/pages/auth/register/RegisterContent.dart
@@ -0,0 +1,219 @@
+import 'package:flutter/material.dart';
+import 'package:indriver_clone_flutter/src/presentation/widgets/DefaultButton.dart';
+import 'package:indriver_clone_flutter/src/presentation/widgets/DefaultTextFieldOutlined.dart';
+
+class RegisterContent extends StatelessWidget {
+  const RegisterContent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(         
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          padding: EdgeInsets.only(left: 12),
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [
+                Color.fromARGB(255, 12, 38, 145),
+                Color.fromARGB(255, 34, 156, 249)
+              ]
+            )
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              _textLoginRotated(context),
+              SizedBox(height: 100),
+              _textRegisterRotated(),
+              SizedBox(height: MediaQuery.of(context).size.height * 0.25)
+            ],
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.only(left: 60, bottom: 35),          
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(35),
+              bottomLeft: Radius.circular(35)
+            ),
+            gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [
+                const Color.fromARGB(255, 236, 233, 242),
+                const Color.fromARGB(255, 245, 245, 245),
+              ]
+            )
+          ),
+          child: Stack(
+            children: [
+              _imageBackground(context),
+              SingleChildScrollView(
+                child: Column(
+                  children: [
+                    _imageBanner(),
+                    DefaultTextFieldOutlined(
+                      text: 'nombre', 
+                      icon: Icons.person_outline,
+                      margin: EdgeInsets.only(left: 50, right: 50, top: 50),
+                    ),
+                    DefaultTextFieldOutlined(
+                      text: 'Apellido', 
+                      icon: Icons.person_2_outlined,
+                      margin: EdgeInsets.only(left: 50, right: 50, top: 15),
+                    ),
+                    DefaultTextFieldOutlined(
+                      text: 'Email', 
+                      icon: Icons.email_outlined,
+                      margin: EdgeInsets.only(left: 50, right: 50, top: 15),
+                    ),
+                    DefaultTextFieldOutlined(
+                      text: 'Telefono', 
+                      icon: Icons.phone_outlined,
+                      margin: EdgeInsets.only(left: 50, right: 50, top: 15),
+                    ),
+                    DefaultTextFieldOutlined(
+                      text: 'Password', 
+                      icon: Icons.lock_outline,
+                      margin: EdgeInsets.only(left: 50, right: 50, top: 15),
+                    ),
+                    DefaultTextFieldOutlined(
+                      text: 'Confirmar password', 
+                      icon: Icons.lock_outline,
+                      margin: EdgeInsets.only(left: 50, right: 50, top: 15),
+                    ),
+                    DefaultButton(
+                      text: 'Crear usuario',
+                      margin: EdgeInsets.only(top: 30, left: 60, right: 60),
+                    ),
+                    SizedBox(height: 25),
+                    _separatorOr(),
+                    SizedBox(height: 10),
+                    _textIAlreadyHaveAccount(context)
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),        
+      ],
+    );
+  }
+
+  Widget _imageBackground(BuildContext context){
+    return Container(
+      alignment: Alignment.center,      
+      child: Image.asset(
+        'assets/img/destination.png',
+        width: MediaQuery.of(context).size.width * 0.6,
+        height: MediaQuery.of(context).size.height * 0.4,
+        opacity: AlwaysStoppedAnimation(0.2),
+      )
+    );
+  }
+
+  Widget _textIAlreadyHaveAccount(BuildContext context){
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          'Ya tienes cuenta',
+          style: TextStyle(
+            color: Colors.grey,
+            fontSize: 16
+          ),
+        ),
+        SizedBox(width: 5),
+        GestureDetector(
+          onTap: () => {
+            Navigator.pushNamed(context, 'login')
+          },
+          child: Text(
+            'Inicia sesion',
+            style: TextStyle(
+              color: Colors.grey[900],
+              fontWeight: FontWeight.bold,
+              fontSize: 16
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _separatorOr(){
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          width: 25,
+          height: 1,
+          color: Colors.black,
+          margin: EdgeInsets.only(right: 5),
+        ),
+        Text(
+          'O',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 17
+          ),
+        ),
+        Container(
+          width: 25,
+          height: 1,
+          color: Colors.black,
+          margin: EdgeInsets.only(left: 5),
+        ),
+      ],
+    );
+  }
+
+  Widget _imageBanner() {
+    return Container(
+      margin: EdgeInsets.only(top:60),
+      alignment: Alignment.center,
+      child: Image.asset(
+        'assets/img/delivery.png',
+        width: 180,
+        height: 180,
+      ),      
+    );
+  }
+
+  Widget _textRegisterRotated(){
+    return RotatedBox(
+      quarterTurns: 1,
+      child: Text(
+        'Register',
+        style: TextStyle(
+          fontSize: 30,
+          color: Colors.white,
+          fontWeight: FontWeight.bold
+        )
+      )      
+    );
+  }
+
+  Widget _textLoginRotated(BuildContext context){
+    return GestureDetector(
+      onTap: () => {
+        Navigator.pushNamed(context, 'login')
+      },
+      child: RotatedBox(
+        quarterTurns: 1,
+        child: Text(
+          'Log In',
+          style: TextStyle(
+            fontSize: 24,
+            color: Colors.white          
+          )
+        )      
+      ),
+    );
+  }
+}
--- lib/src/presentation/widgets/DefaultTextFieldOutlined.dart
+++ lib/src/presentation/widgets/DefaultTextFieldOutlined.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
-class DefaultTextField extends StatelessWidget {
+class DefaultTextFieldOutlined extends StatelessWidget {
   final String text;
   final IconData icon;
   final EdgeInsetsGeometry margin;
 
-  const DefaultTextField({super.key, 
+  const DefaultTextFieldOutlined({super.key, 
     required this.text,
     required this.icon,
     this.margin = const EdgeInsets.only(top: 50, left: 20, right: 20),
@@ -17,7 +17,7 @@ class DefaultTextField extends StatelessWidget {
       height: 50,
       margin: margin,
       decoration: BoxDecoration(
-        color: Colors.white,
+        color: const Color.fromRGBO(255, 252, 252, 0.2),
         borderRadius: BorderRadius.only(
           topLeft: Radius.circular(15),
           bottomRight: Radius.circular(15),
@@ -26,7 +26,18 @@ class DefaultTextField extends StatelessWidget {
       child: TextFormField(
         decoration: InputDecoration(
           label: Text(text),
-          border: InputBorder.none,
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: const Color.fromARGB(255, 37, 100, 151),
+              width: 1
+            )
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.cyan,
+              width: 1
+            )
+          ),
           prefixIcon: Container(
             margin: EdgeInsets.only(top: 10),
             child: Wrap(
